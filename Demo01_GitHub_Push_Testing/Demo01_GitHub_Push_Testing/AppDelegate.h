d831182bf8be17179ce3f6d58af30effd205a01c
norman.tang <nomanland2010@163.com>
2016-02-25 17:30:53 +0800

    pull request!

---
+++ Demo01_GitHub_Push_Testing/Demo01_GitHub_Push_Testing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo01_GitHub_Push_Testing
+//
+//  Created by Norman Tang on 16/2/25.
+//  Copyright © 2016年 Norman Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
